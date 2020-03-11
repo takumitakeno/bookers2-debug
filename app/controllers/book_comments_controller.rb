1f359e7b1807d05d354cfd4593aaf99b63278ef3
takumitakeno <matchball.shine@gmail.com>
2020-03-11 06:08:22 +0000

    [Finish]課題６

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,21 +1,19 @@
 class BookCommentsController < ApplicationController
 	def create
 		@book = Book.find(params[:book_id])
-		# binding.pry
 		@book_comment = current_user.book_comments.new(book_comment_params)
 		# book_comment = BookComment.new(book_comment_params)
 		# book_comment.user_id = current_user.id
 		# レコードに入れる値をいれてあげているだけ
 		# １対他の関係なので、モデルであっても複数形となる
-		# この部分の説明をしてほしい
 		@book_comment.book_id = @book.id
 		if @book_comment.save
 		# binding.pry
-		redirect_to book_path(@book)
+		@book_comments = @book.book_comments
+		# binding.pry
 	    else
 	    @booknew = Book.new
 	    @user = @book.user
-	    @book_comments = @book.book_comments
 	    # @book_comment = BookComment.newを書いてしまうと、データが初期化されてしまう。上から順に読まれることを意識する。
 	    render :"books/show"
 	    end
@@ -23,10 +21,10 @@ class BookCommentsController < ApplicationController
 	end
 	def destroy
 		# binding.pry
-		book = Book.find(params[:id])
-		book_comment = current_user.book_comments.find_by(book_id: book.id)
-		book_comment.destroy
-		redirect_to book_path(book_comment.book)
+		@book = Book.find(params[:book_id])
+		@book_comment = current_user.book_comments.find_by(id: params[:id])
+		@book_comments = @book.book_comments
+		@book_comment.destroy
 	end
 	private
     def book_comment_params
